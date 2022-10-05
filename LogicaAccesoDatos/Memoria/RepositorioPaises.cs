c404f4de0e96613a4b8784bc808e5cbced3e7dff
Sabrina Taramasco <sabrinataramasco@hotmail.com>
2022-10-05 20:40:09 -0300

    Primer commit

---
+++ LogicaAccesoDatos/Memoria/RepositorioPaises.cs
@@ -0,0 +1,57 @@
+﻿using LogicaNegocio;
+using LogicaNegocio.InterfacesRepositorios;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicaAccesoDatos.Memoria
+{
+    public class RepositorioPaises : IRepositorioPaises
+    {
+
+        public static List<Pais> paises { get; set; } = new List<Pais>();
+        public static int UltimoId { get; set; }
+        public void Add(Pais nuevo)
+        {
+            nuevo.Validar();
+            nuevo.Id = ++UltimoId;
+            paises.Add(nuevo);
+        }
+
+        public IEnumerable<Pais> FindAll()
+        {
+            return paises;
+        }
+
+        public Pais FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Pais FindPaisByCodigo()
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public IEnumerable<Pais> GetPaisesByRegion()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Pais obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ValidarEliminacion()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LogicaAccesoDatos/Memoria/RepositorioSelecciones.cs
@@ -0,0 +1,43 @@
+﻿using ObligatorioP3;
+using LogicaNegocio.InterfacesRepositorios;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicaAccesoDatos.Memoria
+{
+    public class RepositorioSelecciones : IRepositorioSelecciones
+    {
+        public static List<Seleccion> selecciones { get; set; } = new List<Seleccion>();
+
+        public void Add(Seleccion obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Seleccion> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Seleccion FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Seleccion obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ValidarEliminacion()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
